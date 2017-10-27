ac3320eaf020c181c7f4c4f1710b60760f15f818
James Fox <James Fox>
2017-10-27 16:08:08 +0100

    adding pre-reqs for logging

--- CifFile/Container/ServiceProvider.cs
+++ CifFile/Container/ServiceProvider.cs
@@ -1,6 +1,9 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 using System;
+using System.IO;
 
 using CifFile.Lib;
 
@@ -12,8 +15,16 @@ namespace CifFile
 
         public static void Build()
         {
+            IConfiguration loggingConfiguration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("logging.json", optional: false, reloadOnChange: true)
+                .Build();
+
             _serviceProvider = new ServiceCollection()
-                .AddLogging()
+                .AddLogging(builder =>
+                    {
+                        builder.AddConfiguration(loggingConfiguration.GetSection("Logging"));
+                    })
                 .AddSingleton<IInputStreamFactory, InputStreamFactory>()
                 .AddSingleton<IFileSystem, FileSystem>()
                 .AddSingleton<IOutputWriter, CsvFileOutputWriter>()
